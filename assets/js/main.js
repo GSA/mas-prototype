cf352b914a06bb47dcc23feeb56bcc17b9596c60
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2022-07-19 13:01:02 -0600

    make sure stuff works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   console.log('Hello world!');
+
+  $form = document.querySelector('#test-form');
+  $form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    document.querySelector('#output').innerText = JSON.stringify(data, null, 2);
+  })
 });
